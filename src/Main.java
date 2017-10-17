560ce23b2edccff2b915798e1224eef1f6e43414
Xaioting <ting02280@gmail.com>
2017-10-17 23:40:41 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String str;
+        int a,b,c,d;
+        str=in .nextLine();
+        a=((str.charAt(0)-'0')+7)%10;
+        b=((str.charAt(1)-'0')+7)%10;
+        c=((str.charAt(2)-'0')+7)%10;
+        d=((str.charAt(3)-'0')+7)%10;
+        //abcd
+        //cdab 1234
+        System.out.printf("%d%d%d%d",c,d,a,b);
+    }
+}
